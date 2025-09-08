71611a7188a8c590c149ccd5a06bd392b648a26b
el <alizaummuelr16@gmail.com>
2025-09-08 09:36:15 +0800

    MainCompany.java

--- MainCompany.java
+++ MainCompany.java
@@ -1,36 +1,39 @@
-package aksesModifier;
+package company;
 
 import java.util.Scanner;
 
 public class MainCompany {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
 
+        System.out.println("\n====INPUT DATA PEGAWAI====");
         System.out.print("Masukkan Nama Pegawai\t: ");
-        String nama = scanner.nextLine();
-
+        String nama = input.nextLine();
         System.out.print("Masukkan ID Pegawai\t: ");
-        String idPegawai = scanner.nextLine();
-
+        String idPegawai = input.nextLine();
         System.out.print("Masukkan Gaji Pokok\t: ");
-        double gajiPokok = scanner.nextDouble();
+        double gajiPokok = input.nextDouble();
+        System.out.println("\n=============================================");
 
+        input.nextLine();
+        
         System.out.print("Apakah pegawai ini seorang Manajer? (y/n): ");
-        char isManajer = scanner.next().toLowerCase().charAt(0);
+        String isManajer = input.nextLine();
 
-        Pegawai pegawai;
+        pegawai p;
 
-        if (isManajer == 'y') {
-            scanner.nextLine();
+        if (isManajer.equalsIgnoreCase("y")) {
             System.out.print("Masukkan Divisi\t\t: ");
-            String divisi = scanner.nextLine();
-            pegawai = new Manajer(nama, idPegawai, gajiPokok, divisi);
+            String divisi = input.nextLine();
+            p = new manajer(nama, idPegawai, gajiPokok, divisi); 
         } else {
-            pegawai = new Pegawai(nama, idPegawai, gajiPokok);
+            p = new pegawai(nama, idPegawai, gajiPokok);
         }
 
         System.out.println("\n====DATA PEGAWAI====");
-        pegawai.tampilkanData();
+        p.tampilkanData();
+        
+        input.close();
+        }
     }
-
 }
