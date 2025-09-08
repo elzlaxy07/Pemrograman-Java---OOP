8631644f8d757c61ecc050493b07637823e0d5e4
el <alizaummuelr16@gmail.com>
2025-09-08 08:52:26 +0800

    Manajer.java

--- manajer.java
+++ manajer.java
@@ -1,20 +1,17 @@
 package aksesModifier;
 
-public class Manajer extends Pegawai {
-    String divisi; // default access modifier
+public class manajer extends pegawai {
+    String divisi;
 
-    // Constructor
-    public Manajer(String nama, String idPegawai, double gajiPokok, String divisi) {
-        super(nama, idPegawai, gajiPokok); // memanggil constructor parent
+    public manajer(String nama, String idPegawai, double gajiPokok, String divisi) {
+        super(nama, idPegawai, gajiPokok);
         this.divisi = divisi;
     }
 
-    // Override method tampilkanData()
     @Override
     public void tampilkanData() {
         System.out.println("Nama\t\t: " + nama);
         System.out.println("ID Pegawai\t: " + idPegawai);
         System.out.println("Divisi\t\t: " + divisi);
-        // Tidak menampilkan gaji pokok sesuai instruksi
     }
 }
